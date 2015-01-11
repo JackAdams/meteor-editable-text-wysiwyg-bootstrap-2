c4db3809c06f20dd4a26ea747d16ebc5126707f8
Brent Abrahams <brent_abrahams@yahoo.com>
2015-01-11 16:42:09 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   
   api.use('babrahams:editable-text@0.6.7', ['client','server']);
   api.imply('babrahams:editable-text');
-  api.use('babrahams:editable-text-wysiwyg@0.3.1', 'client');
+  api.use('babrahams:editable-text-wysiwyg@0.3.2', 'client');
   api.use('templating', 'client');
   api.use('blaze', 'client');
   api.use('spacebars', 'client');
